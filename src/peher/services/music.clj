f12ac90b5f83db73bfa17add007f96bdec62c780
Taha Husain <taha@bigbinary.com>
2019-04-28 13:52:20 +0530

    add complete list of ragas - display title on index page

--- src/peher/services/music.clj
+++ src/peher/services/music.clj
@@ -3,7 +3,8 @@
            [peher.services.raga :as r]
            [clj-http.client     :as http]
            [cheshire.core       :as json]
-           [aero.core           :as conf]))
+           [aero.core           :as conf]
+           [clojure.string      :as s]))
 
 (def config (conf/read-config (clojure.java.io/resource "config.edn")))
 (def api-key (:api-key config))
@@ -23,19 +24,21 @@
 
 (defn fetch-list-from-yt
  []
- (http/get "https://www.googleapis.com/youtube/v3/search"
-           {:accept :json,
-            :query-params {"q"    (first (current-ragas)),
-                           "part" "snippet"
-                           "key"  api-key
-                           "maxResults" 50}}))
+ (let [ragas (current-ragas)
+       q-str (first ragas)]
+  (http/get "https://www.googleapis.com/youtube/v3/search"
+            {:accept :json,
+             :query-params {"q"          q-str,
+                            "part"       "snippet"
+                            "key"        api-key
+                            "maxResults" 50}})))
 
 (defn parse-music-list
  [yt-response]
  (let [{body :body} yt-response
        items        (-> body (json/parse-string keyword) :items)]
-   (map #(-> % :id :videoId) items)
-  ))
+   (map #(hash-map :id    (-> % :id :videoId)
+                   :title (-> % :snippet :title)) items)))
 
 (defn music-list
  []
--- src/peher/views/index.clj
+++ src/peher/views/index.clj
@@ -1,13 +1,13 @@
 (ns peher.views.index
-  (:require [hiccup.core :refer :all]))
+  (:require [hiccup.core  :refer :all]))
 
 (defn page
  [list]
  (html
-  [:div {:data-video (first list)
+  [:div {:data-video (:id (first list))
          :data-autoplay "0"
          :data-loop "1"
          :id "youtube-audio"}]
+  [:p (:title (first list))]
   [:script {:src "https://www.youtube.com/iframe_api"}]
-  [:script {:src "https://cdn.rawgit.com/labnol/files/master/yt.js"}]
-  [:h1 "Welcome to Peher"]))
+  [:script {:src "https://cdn.rawgit.com/labnol/files/master/yt.js"}]))
