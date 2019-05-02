7bf78ddadeac8ad3516c5dd97de3a4e9baf9cb00
Taha Husain <tahahussain18@gmail.com>
2019-05-02 20:30:17 +0530

    add music-list api endpoint to fetch ragas in react

--- src/peher/routes.clj
+++ src/peher/routes.clj
@@ -1,8 +1,10 @@
 (ns peher.routes
   (:require [compojure.route                  :as    route]
             [compojure.core                   :refer [defroutes context GET POST]]
-            [peher.controllers.app-controller :refer [index]]))
+            [cheshire.core                    :refer [generate-string]]
+            [peher.controllers.app-controller :refer [index music-list]]))
 
 (defroutes app-routes
-  (GET "/"  []    (index))
+  (GET "/"           [] (index))
+  (GET "/music-list" [] (generate-string (music-list)))
   (route/not-found "<h1>Page not found</h1>"))
