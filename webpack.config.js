9451e8931c044e69181977265f89ef6d3b4cb8f8
Taha Husain <tahahussain18@gmail.com>
2019-05-02 14:36:10 +0530

    move youtube player in react

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'resources', 'public', 'js'),
     filename: 'bundle.js'
-  }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  mode: 'none'
 }
