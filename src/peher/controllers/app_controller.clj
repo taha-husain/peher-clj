5b658a547bcf998891dca8e451d1d3aa62edcb4d
Taha Husain <6991364+taha-husain@users.noreply.github.com>
2019-05-09 18:30:18 +0530

    fix issue with yt-player where tracks did not play intermittently (#7)

--- src/peher/controllers/app_controller.clj
+++ src/peher/controllers/app_controller.clj
@@ -1,11 +1,9 @@
 (ns peher.controllers.app-controller
  (:require [peher.views.index :as index]
-           [peher.services.music :as m]))
+           [peher.services.music :as m]
+           [cheshire.core :refer [generate-string]]))
 
 (defn index
  []
- (index/page))
-
-(defn music-list
- []
- (m/music-list))
+ (index/page
+  (generate-string (m/music-list))))
